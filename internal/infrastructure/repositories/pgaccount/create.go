ad0a6c9c552007da264344d193159bc073266d53
Maksim <lyapkin992@gmail.com>
2026-04-22 18:35:11 +0500

    handle account_role create and update

--- internal/infrastructure/repositories/pgaccount/create.go
+++ internal/infrastructure/repositories/pgaccount/create.go
@@ -4,15 +4,41 @@ import (
 	"context"
 
 	"github.com/lyapkin/shop/auth/internal/app/domain"
+	"github.com/lyapkin/shop/auth/internal/storage/postgres"
 )
 
 func (r *accountRepo) Create(ctx context.Context, input *domain.Account) (*domain.Account, error) {
+	tx, err := r.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, postgres.BuildErr(err, "")
+	}
+	defer tx.Rollback()
+
 	query := `INSERT INTO account (id, name, email, password)
 	VALUES ($1, $2, $3, $4)`
-
-	_, err := r.db.ExecContext(ctx, query, input.ID, input.Name, input.Email, input.Password)
+	_, err = tx.ExecContext(ctx, query, input.ID, input.Name, input.Email, input.Password)
 	if err != nil {
-		return nil, buildErr(err)
+		return nil, postgres.BuildErr(err, table)
+	}
+
+	query = `
+	INSERT INTO account_role (account_id, role_id)
+	VALUES ($1, $2)
+	`
+	stmt, err := tx.PrepareContext(ctx, query)
+	if err != nil {
+		return nil, postgres.BuildErr(err, "account_role")
+	}
+	defer stmt.Close()
+	for _, role := range input.Roles {
+		_, err := stmt.ExecContext(ctx, input.ID, role.ID)
+		if err != nil {
+			return nil, postgres.BuildErr(err, "account_role")
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, postgres.BuildErr(err, "")
 	}
 
 	return input, nil
--- internal/infrastructure/repositories/pgaccount/update.go
+++ internal/infrastructure/repositories/pgaccount/update.go
@@ -8,14 +8,19 @@ import (
 )
 
 func (r *accountRepo) Update(ctx context.Context, input *domain.Account) (*domain.Account, error) {
+	tx, err := r.db.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, postgres.BuildErr(err, "")
+	}
+	defer tx.Rollback()
+
 	query := `
 	UPDATE account SET name = $2, email = $3
 	WHERE id = $1
 	RETURNING id, name, email
 	`
-
 	var result domain.Account
-	if err := r.db.QueryRowContext(ctx, query, input.Name, input.Email).Scan(
+	if err := tx.QueryRowContext(ctx, query, input.ID, input.Name, input.Email).Scan(
 		&result.ID,
 		&result.Name,
 		&result.Email,
@@ -23,7 +28,35 @@ func (r *accountRepo) Update(ctx context.Context, input *domain.Account) (*domai
 		return nil, postgres.BuildErr(err, table)
 	}
 
-	// TODO: update account role
+	query = `
+	DELETE FROM account_role WHERE account_id = $1
+	`
+	_, err = tx.ExecContext(ctx, query, input.ID)
+	if err != nil {
+		return nil, postgres.BuildErr(err, "account_role")
+	}
+
+	query = `
+	INSERT INTO account_role (account_id, role_id)
+	VALUES ($1, $2)
+	`
+	stmt, err := tx.PrepareContext(ctx, query)
+	if err != nil {
+		return nil, postgres.BuildErr(err, "account_role")
+	}
+	defer stmt.Close()
+	for _, role := range input.Roles {
+		_, err := stmt.ExecContext(ctx, input.ID, role.ID)
+		if err != nil {
+			return nil, postgres.BuildErr(err, "account_role")
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return nil, postgres.BuildErr(err, "")
+	}
+
+	result.Roles = input.Roles
 
 	return &result, nil
 }
